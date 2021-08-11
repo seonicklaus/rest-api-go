92351d3208f36781925d4aaf456cd320593130b0
seonicklaus <seo.nicklaus@gmail.com>
2021-08-11 23:12:05 +0800

    Added controller to make backend independent

---
+++ controller/post-controller.go
@@ -0,0 +1,64 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/seonicklaus/rest-api-go/entity"
+	"github.com/seonicklaus/rest-api-go/errors"
+	"github.com/seonicklaus/rest-api-go/service"
+)
+
+type PostController interface {
+	GetPosts(w http.ResponseWriter, r *http.Request)
+	AddPost(w http.ResponseWriter, r *http.Request)
+}
+
+type controller struct{}
+
+var (
+	postService service.PostService
+)
+
+func NewPostController(service service.PostService) PostController {
+	postService = service
+	return &controller{}
+}
+
+func (*controller) GetPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+	posts, err := postService.FindAll()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errors.ServiceError{Message: "error getting the posts"})
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(posts)
+}
+
+func (*controller) AddPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	var post entity.Post
+	err1 := json.NewDecoder(r.Body).Decode(&post)
+	if err1 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errors.ServiceError{Message: "error unmarshalling data"})
+		return
+	}
+	err2 := postService.Validate(&post)
+	if err2 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errors.ServiceError{Message: err2.Error()})
+		return
+	}
+
+	result, err3 := postService.Create(&post)
+	if err3 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errors.ServiceError{Message: "error saving post"})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
+}
