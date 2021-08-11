5c9f449cf41b327cc916a15054913c8d4f081bcc
seonicklaus <seo.nicklaus@gmail.com>
2021-08-11 23:10:17 +0800

    Added post-service.go to make backend independent of services

---
+++ service/post-service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"errors"
+	"math/rand"
+
+	"github.com/seonicklaus/rest-api-go/entity"
+	"github.com/seonicklaus/rest-api-go/repository"
+)
+
+type PostService interface {
+	Validate(post *entity.Post) error
+	Create(post *entity.Post) (*entity.Post, error)
+	FindAll() ([]entity.Post, error)
+}
+
+type service struct{}
+
+var (
+	repo repository.PostRepository
+)
+
+func NewPostService(repository repository.PostRepository) PostService {
+	repo = repository
+	return &service{}
+}
+
+func (*service) Validate(post *entity.Post) error {
+	if post == nil {
+		err := errors.New("post is nil")
+		return err
+	}
+	if post.Title == "" {
+		err := errors.New("post title is nil")
+		return err
+	}
+
+	return nil
+}
+
+func (*service) Create(post *entity.Post) (*entity.Post, error) {
+	post.ID = rand.Int63()
+	return repo.Save(post)
+}
+
+func (*service) FindAll() ([]entity.Post, error) {
+	return repo.FindAll()
+}
