e5864412fd7912517213c1ce5079a495c3c840b8
seonicklaus <seo.nicklaus@gmail.com>
2021-08-12 01:23:14 +0800

    Added code tester for service module

---
+++ service/post-service_test.go
@@ -0,0 +1,79 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/seonicklaus/rest-api-go/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (mock *MockRepository) Save(post *entity.Post) (*entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Post), args.Error(1)
+}
+
+func (mock *MockRepository) FindAll() ([]entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]entity.Post), args.Error(1)
+}
+
+func TestFindAll(t *testing.T) {
+	mockRepo := new(MockRepository)
+	var identifier int64 = 1
+	post := entity.Post{ID: identifier, Title: "A", Text: "B"}
+
+	// Setup expectations
+	mockRepo.On("FindAll").Return([]entity.Post{post}, nil)
+
+	testService := NewPostService(mockRepo)
+
+	result, _ := testService.FindAll()
+
+	// Mock Assertion: Behavioral
+	mockRepo.AssertExpectations(t)
+
+	// Data Assertion
+	assert.Equal(t, identifier, result[0].ID)
+	assert.Equal(t, "A", result[0].Title)
+	assert.Equal(t, "B", result[0].Text)
+}
+
+func TestCrate(t *testing.T) {
+	mockRepo := new(MockRepository)
+	post := entity.Post{Title: "A", Text: "B"}
+
+	// Setup Expectation
+	mockRepo.On("Save").Return(&post, nil)
+
+	testService := NewPostService(mockRepo)
+	result, err := testService.Create(&post)
+
+	mockRepo.AssertExpectations(t)
+
+	assert.NotNil(t, result.ID)
+	assert.Equal(t, "A", result.Title)
+	assert.Equal(t, "B", result.Text)
+	assert.Nil(t, err)
+}
+
+func TestValidateEmptyPost(t *testing.T) {
+	testService := NewPostService(nil)
+	err := testService.Validate(nil)
+	assert.NotNil(t, err)
+	assert.Equal(t, "post is nil", err.Error())
+}
+
+func TestValidateEmptyPostTitle(t *testing.T) {
+	post := entity.Post{ID: 1, Title: "", Text: "B"}
+	testService := NewPostService(nil)
+	err := testService.Validate(&post)
+	assert.NotNil(t, err)
+	assert.Equal(t, "post title is nil", err.Error())
+}
