16cf123091c497f8948d8e4c40bb3f58bedeba5d
seonicklaus <seo.nicklaus@gmail.com>
2021-08-11 21:01:12 +0800

    Added and completed APIs

--- repository/post-repo.go
+++ repository/post-repo.go
@@ -1,11 +1,88 @@
 package repository
 
-import "github.com/seonicklaus/rest-api-go/entity"
+import (
+	"context"
+	"log"
 
-type PostsRepository interface {
+	"github.com/seonicklaus/rest-api-go/entity"
+
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/iterator"
+)
+
+const (
+	projectID      string = "rest-api-be2c9"
+	collectionName string = "posts"
+)
+
+type PostRepository interface {
 	Save(post *entity.Post) (*entity.Post, error)
 	FindAll() ([]entity.Post, error)
 }
 
-type report struct {
+type repo struct{}
+
+// New Post Repository
+func NewPostsRepository() PostRepository {
+	return &repo{}
+}
+
+func (*repo) Save(post *entity.Post) (*entity.Post, error) {
+	ctx := context.Background()
+
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create a Firestore Client: %v", err)
+		return nil, err
+	}
+
+	defer client.Close()
+
+	_, _, err = client.Collection(collectionName).Add(ctx, map[string]interface{}{
+		"ID":    post.ID,
+		"Title": post.Title,
+		"Text":  post.Text,
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to add a new post: %v", err)
+		return nil, err
+	}
+
+	return post, nil
+}
+
+func (*repo) FindAll() ([]entity.Post, error) {
+	ctx := context.Background()
+
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create a Firestore Client: %v", err)
+		return nil, err
+	}
+
+	defer client.Close()
+
+	var posts []entity.Post
+	iter := client.Collection(collectionName).Documents(ctx)
+
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate the list of posts: %v", err)
+			return nil, err
+		}
+
+		post := entity.Post{
+			ID:    doc.Data()["ID"].(int64),
+			Title: doc.Data()["Title"].(string),
+			Text:  doc.Data()["Text"].(string),
+		}
+		posts = append(posts, post)
+	}
+
+	return posts, nil
 }
